10d49e8d3af87380b26d89e020758275c9882870
=synthia <=synthia.mist@gmail.com>
2024-09-17 11:57:36 +0930

    Modified Question 3 with Proper Solution
    Signed-off-by: = Synthia Islam

--- Ques_3.py
+++ Ques_3.py
@@ -1,34 +1,123 @@
-# Revealing the key
-total = 0
+'''Question 3'''
 
-for i in range(5):
-    for j in range(3):
-        if i + j == 5:
-            total += i + j
+
+def find_key():
+    '''Find Key Function'''
+    total = 0
+    for i in range(5):
+        for j in range(3):
+            if i+j == 5:
+                total += i + j
+            else:
+                total -= i - j
+
+    counter = 0
+    while counter < 5:
+        if total < 13:
+            total += 1
+        elif total > 13:
+            total -= 1
         else:
-            total -= i - j
+            counter += 2
 
-counter = 0
-while counter < 5:
-    if total < 13:
-        total += 1
-    elif total > 13:
-        total -= 1
-    else:
-        counter += 2
+    print("\nKey: " + str(total))
+    return total
 
-print(total)  # This value might be the key for the encryption function
 
-# Initialize the global variable at the start of your script
-global_variable = 0
+def decrypt(encrypted_text, key):
+    '''decrypt method'''
+    decrypted_text = ""
+    for char in encrypted_text:
+        if char.isalpha():  # Check if the character is alphabetical
+            shifted = ord(char) - key
+            if char.islower():  # Wrap around for lowercase letters
+                if shifted < ord('a'):
+                    shifted += 26
+            elif char.isupper():  # Wrap around for uppercase letters
+                if shifted < ord('A'):
+                    shifted += 26
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char  # Non-alphabetic characters are unchanged
+    return decrypted_text
 
+
+def main():
+    '''Main function'''
+    # Example encrypted code and a hypothetical key
+    key = find_key()
+
+    encrypted_code = """
+    tybony_inevnoyr = 100
+    zl_qvpg = {'xrl1': 'inyhr1', 'xrl2': 'inyhr2', 'xrl3': 'inyhr3'}
+
+    qrs cebprff_ahzoref():
+        tybony tybony_inevnoyr
+        ybpny_inevnoyr = 5
+        ahzoref = [1, 2, 3, 4, 5]
+
+        juvyr ybpny_inevnoyr > 0:
+            vs ybpny_inevnoyr % 2 == 0:
+                ahzoref.erzbir(ybpny_inevnoyr)
+            ybpny_inevnoyr -= 1
+
+        erghea ahzoref
+
+    zl_frg = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+    erfhyg = cebprff_ahzoref(ahzoref=zl_frg)
+
+    qrs zbqvsl_qvpg():
+        ybpny_inevnoyr = 10
+        zl_qvpg['xrl4'] = ybpny_inevnoyr
+
+    zbqvsl_qvpg(5)
+
+    qrs hcqngr_tybony():
+        tybony tybony_inevnoyr
+        tybony_inevnoyr += 10
+
+    sbe v va enatr(5):
+        cevag(v)
+        v += 1
+
+    vs zl_frg vf abg Abar naq zl_qvpg['xrl4'] == 10:
+        cevag("Pbaqvgvba zrg!")
+
+    vs 5 abg va zl_qvpg:
+        cevag("5 abg sbhaq va gur qvpgvbanel!")
+
+    cevag(tybony_inevnoyr)
+    cevag(zl_qvpg)
+    cevag(zl_frg)
+    """
+
+    # Decrypt the code
+    original_code = decrypt(encrypted_code, key)
+    print("Decrypted Original Code:")
+    print("---------------")
+    print(original_code)
+    print("\nCorrected Code With Comments:")
+    print("-------------------------------")
+
+
+# Driver code
+if __name__ == '__main__':
+    # Encryption-decryption Code
+    main()
+
+
+# Correct the Errors and Provide the Comments in the Original Code
+global_variable = 100
 my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 
 
-def process_numbers():
-    global global_variable  # Declare that we will use the global variable
+def process_numbers(numbers):  # Added 'numbers' as a parameter
+    '''process_numbers function'''  # Added Function Docstring
+    # Removed global definition of global_variable as there is no use of it
+    # global global_variable
     local_variable = 5
-    numbers = [1, 2, 3, 4, 5]
+    # Removed initialization of 'numbers' as we have used it as a parameter
+    # numbers = [1, 2, 3, 4, 5]
 
     while local_variable > 0:
         if local_variable % 2 == 0:
@@ -38,58 +127,86 @@ def process_numbers():
     return numbers
 
 
+# Sets automatically remove duplicates, initial set defined with duplicates unnecessarily
+# my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
 my_set = {1, 2, 3, 4, 5}
-result = process_numbers()  # Call function to process numbers
+result = process_numbers(numbers=my_set)
 
 
-def multiply_dept():
+def modify_dict():
+    '''Method of Modifying Dictionary'''  # Added Method Docstring
     local_variable = 10
     my_dict['key4'] = local_variable
 
 
-multiply_dept()
+modify_dict()  # Removed the erroneous parameter
 
 
 def update_global():
+    '''Method of Updating Global variable'''  # Added Method Docstring
     global global_variable
     global_variable += 10
-# Print each number from 0 to 4
+
+
+update_global()  # Correctly call the function to update global variable
 
 
 for i in range(5):
     print(i)
+    # Incrementing 'i' inside the loop has no effect due to the nature of Python loops, so removing it
+    # i += 1
 
-# Check if my_dict is properly set up and if specific condition holds
 if my_set is not None and my_dict['key4'] == 10:
-    print("Everything's set!")
+    print("Condition met!")
 
-# Check if a specific key is missing in the dictionary
 if 5 not in my_dict:
     print("5 not found in the dictionary!")
 
-# Print the global variable, dictionary, and set
 print(global_variable)
 print(my_dict)
 print(my_set)
 
 
-def caesar_cipher(text, key=13):  # Default key is 13 for ROT13
-    decrypted_text = ""
-    for char in text:
-        if char.isalpha():  # Check if the character is a letter
-            if char.islower():
-                base = ord('a')
-            else:
-                base = ord('A')
-            shifted = (ord(char) - base - key) % 26 + base
-            decrypted_text += chr(shifted)
-        else:
-            decrypted_text += char  # Keep non-alphabet characters unchanged
-    return decrypted_text
+# After Decryption, the below code is original code
+#     global_variable = 100
+#     my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
 
-# Example usage:
+#     def process_numbers():
+#         '''process_numbers function'''
+#         global global_variable
+#         local_variable = 5
+#         numbers = [1, 2, 3, 4, 5]
 
+#         while local_variable > 0:
+#             if local_variable % 2 == 0:
+#                 numbers.remove(local_variable)
+#             local_variable -= 1
 
-encrypted_message = "PQH vf gur orfg havirefvgl"
-decrypted_message = caesar_cipher(encrypted_message)
-print("Decrypted Message:", decrypted_message)
+#         return numbers
+
+#     my_set = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+#     result = process_numbers(numbers=my_set)
+
+#     def modify_dict():
+#         local_variable = 10
+#         my_dict['key4'] = local_variable
+
+#     modify_dict(5)
+
+#     def update_global():
+#         global global_variable
+#         global_variable += 10
+
+#     for i in range(5):
+#         print(i)
+#         i += 1
+
+#     if my_set is not None and my_dict['key4'] == 10:
+#         print("Condition met!")
+
+#     if 5 not in my_dict:
+#         print("5 not found in the dictionary!")
+
+#     print(global_variable)
+#     print(my_dict)
+#     print(my_set)
